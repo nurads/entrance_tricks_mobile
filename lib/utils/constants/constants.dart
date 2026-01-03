8ebf55bc83f0072181a02e7e4d9fc44358e9cb26
Murad <nuradhussen082@gmail.com>
2026-01-03 21:00:24 +0300

    change to prod

--- lib/utils/constants/constants.dart
+++ lib/utils/constants/constants.dart
@@ -1,3 +1,3 @@
 const authTokenStorage = 'authTokenStorage';
-// const defaultApiURL = 'https://api.entrancetricks.com';
-const defaultApiURL = 'http://192.168.1.4:8000';
+const defaultApiURL = 'https://api.entrancetricks.com';
+// const defaultApiURL = 'http://192.168.1.4:8000';
